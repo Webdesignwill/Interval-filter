2a0d0dd3d0ef25b81cc7ae49129005c6fada6762
Webdesignwill <will@webdesignwill.co.uk>
2014-10-15 16:48:04 +0200

    Node environment var

--- config/config.js
+++ config/config.js
@@ -6,5 +6,9 @@ module.exports = {
   development : {
     db : 'mongodb://127.0.0.1:27017/intervalfilter',
     root : rootPath
+  },
+  production : {
+    db : 'mongodb://heroku_app30721403:heroku_app30721403@ds033170.mongolab.com:33170/heroku_app30721403',
+    root : rootPath
   }
 };
